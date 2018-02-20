77603a461c8e4df834b04b52781b390e3e5c594a
Marta Waplak <mwaplak@gmail.com>
2018-02-20 23:38:50 +0100

    Finish js script

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Button(text){
+    this.text = text || 'Hello';
+}
+
+Button.prototype = {
+    
+    create: function() {
+        var self = this;
+        this.$element = $('<button>');
+        this.$element.text(this.text);
+        this.$element.click(function() {
+            alert(self.text);
+        });
+        $('body').append(this.$element);
+    }
+}
+
+var btn1 = new Button('Hello');
+btn1.create();
+
+$('button').css('padding-left', '40px').css('padding-right', '40px').css('padding-top', '20px').css('padding-bottom', '20px').css('background-color', 'white').css('font-weight', 'bold');
